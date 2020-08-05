32feac9e4f6f58543ce783af4a12d6a15d4001e2
Alexander Harris <ademp90@gmail.com>
2020-08-05 11:38:12 -0700

    Fixing non-https endpoint for last.fm and adding a loading spinner  on network request

--- src/components/SongSuggestions.tsx
+++ src/components/SongSuggestions.tsx
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from "react";
+import { Spinner } from "react-bootstrap";
 import { Genre, Track } from "../genres";
 
 const lastFm = "6d4e62c51de1406977401606a49e67a8";
 
-const getSongsForTag = async (tag?: string): Promise<Track[]> => {
-  if (tag === undefined) {
-    return [];
-  }
+const getSongsForTag = async (tag: string): Promise<Track[]> => {
   const response = await fetch(
-    `http://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=${tag}&api_key=${lastFm}&format=json`
+    `https://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=${tag}&api_key=${lastFm}&format=json`
   );
   if (!response.ok) {
     throw new Error("Network response was not ok");
@@ -24,14 +22,29 @@ interface SongSuggestionsProps {
 const SongSuggestions = (props: SongSuggestionsProps) => {
   const { genre } = props;
   const [tracks, setTracks] = useState<Track[] | undefined>();
+  const [showSpinner, setShowSpinner] = useState(false);
 
   useEffect(() => {
     const loadTracks = async (tag?: string) => {
-      const newTracks = await getSongsForTag(tag);
-      setTracks(newTracks);
+      if (tag) {
+        setShowSpinner(true);
+        const newTracks = await getSongsForTag(tag);
+        setTracks(newTracks);
+        setShowSpinner(false);
+      } else {
+        setTracks([]);
+      }
     };
     loadTracks(genre.tag);
   }, [genre]);
+
+  if (showSpinner) {
+    return (
+      <Spinner animation="border" role="status">
+        <span className="sr-only">Loading...</span>
+      </Spinner>
+    );
+  }
 
   if (genre.tag && tracks && tracks.length !== 0) {
     return (
@@ -40,7 +53,9 @@ const SongSuggestions = (props: SongSuggestionsProps) => {
         <ol>
           {tracks.map((track) => (
             <li>
-              <a href={track.url}>{track.name}</a>
+              <a target="_blank" rel="noopener noreferrer" href={track.url}>
+                {track.name}
+              </a>
             </li>
           ))}
         </ol>
